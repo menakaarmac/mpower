cd5251d13c7c0973944845cf5d0b6e67e3178398
menakaudara <menakaudara@gmail.com>
2014-05-11 17:34:50 +0530

    added lable and tbox

--- myapp/myapp/F1/Form2.cs
+++ myapp/myapp/F1/Form2.cs
@@ -16,5 +16,10 @@ namespace myapp.F1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "yPOYO";
+        }
     }
 }
